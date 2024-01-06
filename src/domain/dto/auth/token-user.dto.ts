a0adada7f897962faaabfa123a88b9023e97d375
isakiDev <castillicatrifol@gmail.com>
2024-01-06 15:46:57 -0300

    feat(auth): add token dto

---
+++ src/domain/dto/auth/token-user.dto.ts
@@ -0,0 +1,14 @@
+export class TokenUserDto {
+  constructor (
+    public token: string
+  ) {}
+
+  static create (token: string): [string?, TokenUserDto?] {
+    if (!token) return ['Missing token']
+
+    return [
+      undefined,
+      new TokenUserDto(token)
+    ]
+  }
+}
