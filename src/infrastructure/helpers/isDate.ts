7f1fb2cd7bf5b6d063fce416bc8e27f297acd452
isakiDev <gaspar.c.developer@gmail.com>
2024-01-20 16:30:25 -0300

    update(isDate): handling dates with luxon

--- src/infrastructure/helpers/isDate.ts
+++ src/infrastructure/helpers/isDate.ts
@@ -1,9 +1,9 @@
+import { DateTime } from 'luxon'
+
 export const isDate = (value: string) => {
   if (!value) return false
 
-  const date = new Date(value)
-
-  if (!isNaN(date.getTime())) return true
+  if (DateTime.fromISO(value).isValid) return true
 
   return false
 }
