bfec760315f1854e57c8899350339d1d3718de23
isakidev <gaspar.c.developer@gmail.com>
2024-08-01 02:06:16 -0400

    chore: add build

---
+++ dist/domain/entities/user.entity.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserEntity = void 0;
+class UserEntity {
+    constructor(id, name, email, password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
+exports.UserEntity = UserEntity;
