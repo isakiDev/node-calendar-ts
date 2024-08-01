b94d706b166496f1e77f8824bfb19a0ac9ba33aa
Gaspar Castillo Catrifol <143692299+isakiDev@users.noreply.github.com>
2024-08-01 00:00:48 -0400

    Update corsAdapter.ts

--- src/config/corsAdapter.ts
+++ src/config/corsAdapter.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = ['https://isakidev-calendar.netlify.app']
+const ACCEPTED_ORIGINS = ['https://isakidev-calendar.netlify.app/']
 
 export const corsAdapter = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
   origin: (origin, callback) => {
