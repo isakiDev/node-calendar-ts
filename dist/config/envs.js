e9899d84992f9d169f03e66b96a8c90d069e5040
Gaspar Castillo Catrifol <143692299+isakiDev@users.noreply.github.com>
2024-08-01 02:20:52 -0400

    Delete dist directory

--- dist/config/envs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.envs = void 0;
-const env_var_1 = require("env-var");
-exports.envs = {
-    PORT: (0, env_var_1.get)('PORT').required().asPortNumber(),
-    DB_CNN: (0, env_var_1.get)('DB_CNN').required().asString(),
-    JWT_SEED: (0, env_var_1.get)('JWT_SEED').required().asString()
-};
