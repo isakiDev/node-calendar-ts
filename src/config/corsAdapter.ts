a01a2f7617c98df4a71bcc3a35370b600708e900
isakidev <gaspar.c.developer@gmail.com>
2024-08-01 16:29:52 -0400

    feat: update cors adapter

--- src/config/corsAdapter.ts
+++ src/config/corsAdapter.ts
@@ -6,5 +6,8 @@ export const corsAdapter = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors
   origin: (origin, callback) => {
     if (!origin || acceptedOrigins?.includes(origin)) { callback(null, true); return }
     callback(new Error('Not allowed by Cors'))
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  credentials: true
 })
