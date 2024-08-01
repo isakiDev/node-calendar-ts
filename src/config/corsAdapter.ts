fe2f4e4bb7041b9c5a08fced2851b4e9f976dd8d
Gaspar Castillo Catrifol <143692299+isakiDev@users.noreply.github.com>
2024-08-01 00:15:22 -0400

    Update corsAdapter.ts

--- src/config/corsAdapter.ts
+++ src/config/corsAdapter.ts
@@ -6,5 +6,7 @@ export const corsAdapter = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors
   origin: (origin, callback) => {
     if (!origin || acceptedOrigins?.includes(origin)) { callback(null, true); return }
     callback(new Error('Not allowed by Cors'))
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 })
