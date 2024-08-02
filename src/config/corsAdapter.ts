4adfeda69e44fd2985cfe8bddf199730d55a7a1e
isakidev <gaspar.c.developer@gmail.com>
2024-08-02 16:40:49 -0400

    chore: delete dist

--- src/config/corsAdapter.ts
+++ src/config/corsAdapter.ts
@@ -1,6 +1,6 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = ['https://isakidev-calendar.netlify.app', 'https://react-calendar-ts.vercel.app']
+const ACCEPTED_ORIGINS = ['https://isakidev-calendar.netlify.app', 'http://isakidev-calendar.netlify.app']
 
 export const corsAdapter = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
   origin: (origin, callback) => {
